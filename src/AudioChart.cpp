90d8ae36aa8839898dd35804b288e0b7d6fad60e
Aaron <noraa7396@gmail.com>
2017-07-17 20:15:20 -0600

    Improvements over AudioChart.cpp, sample rate has been adjusted and colors has been added to the chart

--- src/AudioChart.cpp
+++ src/AudioChart.cpp
@@ -1,7 +1,9 @@
-#include "AudioChart.h"
-#include <vector>
 #include <QGraphicsRectItem>
 #include <QGraphicsScene>
+#include <QGraphicsView>
+#include <vector>
+
+#include "AudioChart.h"
 
 /**
  * @brief Default constructor that sets the rectangles dimensions and maximum ammount of rects that can be drawn
@@ -9,9 +11,10 @@
  * @param parent QObject parent for OBject constructor
  */
 AudioChart::AudioChart(QGraphicsScene *scene, QObject *parent): QObject(parent),
-	scene(scene),
-	rectWidth(5),
-	maxRects(1920/rectWidth - 2) //minus two to avoid rects off screen
+    scene(scene),
+    rectWidth(2),
+    maxRects(1920/rectWidth - 2), //minus two to avoid rects off screen
+    currentColor(0)
 {}
 
 /**
@@ -20,46 +23,49 @@ AudioChart::AudioChart(QGraphicsScene *scene, QObject *parent): QObject(parent),
  */
 void AudioChart::update(int value)
 {
-	//If maxRects hasn't been reached, just add the new rect to end of vector
-	if(info.size() < maxRects){
-		//Pos the rect correctly for positive value
-		if(value > 0){
-			QGraphicsRectItem* newRect = new QGraphicsRectItem(0, 0, rectWidth, value*2);
-			newRect->setPos(info.size()*rectWidth, 1080/2-value*2);
-			newRect->setBrush(QBrush(QColor(255,255,255)));
-			scene->addItem(newRect);
-			info.push_back(newRect);
-		}
-		//Pos the rect correctly for negative value
-		else{
-			QGraphicsRectItem* newRect = new QGraphicsRectItem(0, 0, rectWidth, abs(value*2));
-			newRect->setPos(info.size()*rectWidth, 1080/2);
-			newRect->setBrush(QBrush(QColor(255,255,255)));
-			scene->addItem(newRect);
-			info.push_back(newRect);
-		}
-	}
-	else{
-		//delete the first one and move all the remaining rects to the left
-		delete info.front();
-		info.pop_front();
-		for(std::list<QGraphicsRectItem*>::iterator itr = info.begin(); itr != info.end(); ++itr){
-			(*itr)->setX((*itr)->x()-rectWidth);
-		}
-		//Add new rect correctly
-		if(value > 0){
-			QGraphicsRectItem* newRect = new QGraphicsRectItem(0, 0, rectWidth, value*2);
-			newRect->setPos(info.size()*rectWidth, 1080/2-value*2);
-			newRect->setBrush(QBrush(QColor(255,255,255)));
-			scene->addItem(newRect);
-			info.push_back(newRect);
-		}
-		else{
-			QGraphicsRectItem* newRect = new QGraphicsRectItem(0, 0, rectWidth, abs(value*2));
-			newRect->setPos(info.size()*rectWidth, 1080/2);
-			newRect->setBrush(QBrush(QColor(255,255,255)));
-			scene->addItem(newRect);
-			info.push_back(newRect);
-		}
-	}
+    QColor color = QColor(255,255,255);
+    if(currentColor == 0){
+        color = QColor(255,0,0);
+    }
+    else if(currentColor == 1){
+        color = QColor(0,255,0);
+    }
+    else{
+        color = QColor(0,0,255);
+    }
+
+    //If maxRects hasn't been reached, just add the new rect to end of vector
+    QGraphicsRectItem* newRect = 0;
+    if(info.size() < maxRects){
+        //Pos the rect correctly for positive value
+        if(value > 0){
+            newRect = new QGraphicsRectItem(info.size()*rectWidth, 1080/2-value*2, rectWidth, value*2);
+        }
+        //Pos the rect correctly for negative value
+        else{
+            newRect = new QGraphicsRectItem(info.size()*rectWidth, 1080/2, rectWidth, abs(value*2));
+        }
+    }
+    else{
+        //delete the first one and move all the remaining rects to the left
+        delete info.front();
+        info.pop_front();
+        for(std::list<QGraphicsRectItem*>::iterator itr = info.begin(); itr != info.end(); ++itr){
+            (*itr)->setX((*itr)->x()-rectWidth);
+        }
+        //Add new rect correctly
+        if(value > 0){
+            newRect = new QGraphicsRectItem(info.size()*rectWidth, 1080/2-value*2, rectWidth, value*2);
+        }
+        else{
+            newRect = new QGraphicsRectItem(info.size()*rectWidth, 1080/2, rectWidth, abs(value*2));
+        }
+    }
+
+    newRect->setBrush(QBrush(color));
+    scene->addItem(newRect);
+    info.push_back(newRect);
+
+    currentColor++;
+    currentColor %= 3;
 }
