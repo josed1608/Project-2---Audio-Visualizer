47126c102e7940c23648c586773ed387b984b337
Jose David Vargas Artavia <josed1608@gmail.com>
2017-07-18 08:39:21 -0600

    Updated documentation and changed the layout of the ChoiceWidget.ui

--- docs/html/search/functions_6.js
+++ docs/html/search/functions_6.js
@@ -2,7 +2,6 @@ var searchData=
 [
   ['setbezier',['setBezier',['../class_audio_face.html#a23694360e3916a9a26024e26076e668b',1,'AudioFace']]],
   ['setcolor',['setColor',['../class_audio_circle.html#a6133f7b560babbedae858eb2ac45f147',1,'AudioCircle']]],
-  ['startvisualizationallthree',['startVisualizationAllThree',['../class_choice_widget.html#a3599a0ef7686843d293f6e4000feae4e',1,'ChoiceWidget']]],
   ['startvisualizationaudiochart',['startVisualizationAudioChart',['../class_choice_widget.html#a6db43c9182b6e5f8538c0acca2147d91',1,'ChoiceWidget']]],
   ['startvisualizationaudiocircle',['startVisualizationAudioCircle',['../class_choice_widget.html#a8539bdedb6bd84b154428b133d9e9c7d',1,'ChoiceWidget']]],
   ['startvisualizationaudioface',['startVisualizationAudioFace',['../class_choice_widget.html#a21ee65f8c776d8b367942bd6c6cf2637',1,'ChoiceWidget']]],
